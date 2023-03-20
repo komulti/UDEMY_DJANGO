17fcc5acdc541c1cd82a6f7e470170f923f4adfa
komulti <komulti@gmail.com>
2023-03-20 11:09:28 +0900

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  return HttpRequest("Hello this is a view inside my_app")
